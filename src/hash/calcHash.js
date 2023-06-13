6bef9fa5cfd2113ab1cda801d6ad931d395c4f64
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-06-13 14:53:49 +0300

    feat: add hash calculating

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,13 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import fs from "fs"
+import crypto from "crypto"
 
-await calculateHash();
+const calculateHash = async () => {
+  const filePath = "src/hash/files/fileToCalculateHashFor.txt"
+  const fileData = await fs.promises.readFile(filePath)
+  const hash = crypto.createHash("sha256")
+  hash.update(fileData)
+  const hashHex = hash.digest("hex")
+  console.log(hashHex)
+}
+
+await calculateHash()
