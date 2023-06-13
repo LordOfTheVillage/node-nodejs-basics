5f88d0d1be634e8e889ec41d0194936340570884
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-06-13 14:29:23 +0300

    feat: add args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
-};
+  const flagIndex = "--"
+  const separator = ", "
+  const args = process.argv.slice(2)
+  const flags = args.filter((arg) => arg.startsWith(flagIndex))
+  const result = flags
+    .map((flag) => {
+      const flagName = flag.slice(2)
+      const valueIndex = args.indexOf(flag) + 1
+      return `${flagName} is ${args[valueIndex]}`
+    })
+    .join(separator)
+  console.log(result)
+}
 
-parseArgs();
+parseArgs()
