1937398ccbac721c502383df5d2408dbba3126f0
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-06-13 15:38:58 +0300

    feat: add copy file task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,37 @@
-const copy = async () => {
-    // Write your code here 
-};
+import fs from "fs"
+import path from "path"
 
-await copy();
+const copy = async () => {
+  const sourceDir = "src/fs/files"
+  const targetDir = "src/fs/files_copy"
+  const errorContent = "FS operation failed"
+  const notfoundCode = "ENOENT"
+
+  try {
+    const sourceDirStats = await fs.promises.stat(sourceDir)
+    if (!sourceDirStats.isDirectory()) {
+      throw new Error(errorContent)
+    }
+
+    const targetDirStats = await fs.promises.stat(targetDir)
+    if (targetDirStats.isDirectory()) {
+      throw new Error(errorContent)
+    }
+  } catch (err) {
+    if (err.code !== notfoundCode) {
+      throw err
+    }
+  }
+
+  await fs.promises.mkdir(targetDir)
+  const files = await fs.promises.readdir(sourceDir)
+
+  for (const file of files) {
+    const sourcePath = path.join(sourceDir, file)
+    const targetPath = path.join(targetDir, file)
+
+    await fs.promises.copyFile(sourcePath, targetPath)
+  }
+}
+
+await copy()
