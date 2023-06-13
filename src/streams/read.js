923e628c13c055a380100cb382c7bdc6521c3196
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-06-13 19:33:54 +0300

    feat: add read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
-const read = async () => {
-    // Write your code here 
-};
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from "url"
 
-await read();
+const read = async () => {
+  const currentPath = path.dirname(fileURLToPath(import.meta.url))
+  const fileName = "fileToRead.txt"
+  const folder = "files"
+  const filePath = path.join(currentPath, folder, fileName)
+
+  const stream = fs.createReadStream(filePath, "utf8")
+
+  stream.on("data", (chunk) => {
+    process.stdout.write(chunk)
+  })
+
+  return new Promise((resolve, reject) => {
+    stream.on("end", resolve)
+    stream.on("error", reject)
+  })
+}
+
+await read()
