1250110edcbe0904be892ba9ff4d6fff39e5238f
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-06-13 16:08:46 +0300

    feat: add delete file task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,25 @@
-const remove = async () => {
-    // Write your code here 
-};
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from "url"
 
-await remove();
+const remove = async () => {
+  const currentPath = path.dirname(fileURLToPath(import.meta.url))
+  const errorContent = "FS operation failed"
+  const notfoundCode = "ENOENT"
+  const folder = "files"
+  const fileName = "fileToRemove.txt"
+  const filePath = path.join(currentPath, folder, fileName)
+
+  try {
+    await fs.promises.access(filePath)
+    await fs.promises.unlink(filePath)
+  } catch (err) {
+    if (err.code === notfoundCode) {
+      throw new Error(errorContent)
+    } else {
+      throw err
+    }
+  }
+}
+
+await remove()
