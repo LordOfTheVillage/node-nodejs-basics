bd8b18b7cdfb28a56af0c08d38b20125f4d13e81
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-06-13 16:22:01 +0300

    feat: add list and read tasks

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
-const list = async () => {
-    // Write your code here 
-};
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from "url"
 
-await list();
+const list = async () => {
+  const errorContent = "FS operation failed"
+  const notfoundCode = "ENOENT"
+  const currentPath = path.dirname(fileURLToPath(import.meta.url))
+  const folder = "files"
+  const folderPath = path.join(currentPath, folder)
+
+  try {
+    await fs.promises.access(folderPath)
+    const filenames = await fs.promises.readdir(folderPath)
+    console.log(filenames)
+  } catch (err) {
+    if (err.code === notfoundCode) {
+      throw new Error(errorContent)
+    } else {
+      throw err
+    }
+  }
+}
+
+await list()
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,28 @@
-const read = async () => {
-    // Write your code here 
-};
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from "url"
 
-await read();
+const read = async () => {
+  const errorContent = "FS operation failed"
+  const notfoundCode = "ENOENT"
+  const encoding = "utf8"
+  const currentPath = path.dirname(fileURLToPath(import.meta.url))
+  const folder = "files"
+  const fileName = "fileToRead.txt"
+  const filePath = path.join(currentPath, folder, fileName)
+
+  try {
+    await fs.promises.access(filePath)
+
+    const fileContent = await fs.promises.readFile(filePath, encoding)
+    console.log(fileContent)
+  } catch (err) {
+    if (err.code === notfoundCode) {
+      throw new Error(errorContent)
+    } else {
+      throw err
+    }
+  }
+}
+
+await read()
