27ee7c845450a6faaeeaff24bca8d6d7893e172b
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-06-13 15:52:37 +0300

    feat: add rename tsk

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
-const rename = async () => {
-    // Write your code here 
-};
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from "url"
 
-await rename();
+const rename = async () => {
+  const errorContent = "FS operation failed"
+  const notfoundCode = "ENOENT"
+  const oldFileName = "wrongFilename.txt"
+  const newFileName = "properFilename.md"
+  const folder = "files"
+  const currentPath = path.dirname(fileURLToPath(import.meta.url))
+  const oldFilePath = path.join(currentPath, folder, oldFileName)
+  const newFilePath = path.join(currentPath, folder, newFileName)
+
+  try {
+    await fs.promises.access(oldFilePath)
+    await fs.promises.access(newFilePath)
+
+    throw new Error(errorContent)
+  } catch (err) {
+    if (err.code === notfoundCode) {
+      await fs.promises.rename(oldFilePath, newFilePath)
+    } else {
+      throw err
+    }
+  }
+}
+
+await rename()
