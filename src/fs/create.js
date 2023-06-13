22abfd9f610dcc85ba9d44b8c9c3a92b1e49c615
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-06-13 15:21:18 +0300

    feat: add create file task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
-const create = async () => {
-    // Write your code here 
-};
+import fs from "fs"
 
-await create();
+const create = async () => {
+  const filePath = "src/fs/files/fresh.txt"
+  const fileContent = "I am fresh and young"
+  const errorContent = "FS operation failed"
+  const notfoundCode = "ENOENT"
+
+  try {
+    await fs.promises.access(filePath)
+    throw new Error(errorContent)
+  } catch (err) {
+    if (err.code === notfoundCode) {
+      await fs.promises.writeFile(filePath, fileContent)
+    } else {
+      throw err
+    }
+  }
+}
+
+await create()
