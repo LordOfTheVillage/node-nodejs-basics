4b26a83458bf4bb7d849bd77eefa67810091063c
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-06-13 19:36:37 +0300

    feat: add read task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,21 @@
-const write = async () => {
-    // Write your code here 
-};
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from "url"
 
-await write();
+const write = async () => {
+  const currentPath = path.dirname(fileURLToPath(import.meta.url))
+  const fileName = "fileToWrite.txt"
+  const folder = "files"
+  const filePath = path.join(currentPath, folder, fileName)
+
+  const writeStream = fs.createWriteStream(filePath, "utf8")
+
+  process.stdin.pipe(writeStream)
+
+  return new Promise((resolve, reject) => {
+    writeStream.on("finish", resolve)
+    writeStream.on("error", reject)
+  })
+}
+
+await write()
