5f16976c45da2466b28a41df2aea31b38df8600c
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-06-17 15:43:26 +0300

    feat: add zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
-const compress = async () => {
-    // Write your code here 
-};
+import fs from "fs"
+import zlib from "zlib"
+import path from "path"
+import { fileURLToPath } from "url"
 
-await compress();
+const compress = async () => {
+  const inputFile = "fileToCompress.txt"
+  const outputFile = "archive.gz"
+  const currentPath = path.dirname(fileURLToPath(import.meta.url))
+  const folder = "files"
+  const inputFilePath = path.join(currentPath, folder, inputFile)
+  const outputFilePath = path.join(currentPath, folder, outputFile)
+
+  const readStream = fs.createReadStream(inputFilePath)
+  const writeStream = fs.createWriteStream(outputFilePath)
+  const gzipStream = zlib.createGzip()
+
+  readStream.pipe(gzipStream).pipe(writeStream)
+
+  return new Promise((resolve, reject) => {
+    writeStream.on("finish", resolve)
+    writeStream.on("error", reject)
+  })
+}
+
+await compress()
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
-const decompress = async () => {
-    // Write your code here 
-};
+import fs from "fs"
+import zlib from "zlib"
+import path from "path"
+import { fileURLToPath } from "url"
 
-await decompress();
+const decompress = async () => {
+  const inputFile = "archive.gz"
+  const outputFile = "fileToCompress.txt"
+  const currentPath = path.dirname(fileURLToPath(import.meta.url))
+  const folder = "files"
+  const inputFilePath = path.join(currentPath, folder, inputFile)
+  const outputFilePath = path.join(currentPath, folder, outputFile)
+
+  const readStream = fs.createReadStream(inputFilePath)
+  const writeStream = fs.createWriteStream(outputFilePath)
+  const gunzipStream = zlib.createGunzip()
+
+  readStream.pipe(gunzipStream).pipe(writeStream)
+
+  return new Promise((resolve, reject) => {
+    writeStream.on("finish", resolve)
+    writeStream.on("error", reject)
+  })
+}
+
+await decompress()
