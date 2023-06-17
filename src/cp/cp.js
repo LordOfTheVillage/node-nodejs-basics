73184a5145da85aebdca1af45d8d2bcbf19266ab
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-06-17 18:10:27 +0300

    feat: add child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { spawn } from "child_process"
+import path from "path"
+import { fileURLToPath } from "url"
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const spawnChildProcess = async (args) => {
+  const currentPath = path.dirname(fileURLToPath(import.meta.url))
+  const fileName = "script.js"
+  const folder = "files"
+  const filePath = path.join(currentPath, folder, fileName)
+  const child = spawn("node", [filePath, ...args], { stdio: "pipe" })
+
+  child.stdout.on("data", (data) => {
+    process.stdout.write(data.toString())
+  })
+
+  process.stdin.pipe(child.stdin)
+
+  process.stdin.on("end", () => {
+    child.stdin.end()
+  })
+}
+
+const argumentsToPass = ["someArgument1", "someArgument2"]
+spawnChildProcess(argumentsToPass)
